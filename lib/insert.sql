d97aedf439ab5661ec8077954d1e673858f7d579
Lucy Wall <lhwall@gmail.com>
2018-05-08 20:30:44 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color) VALUES ("Mr. Chocolate", 20, "M", "chocolate");
+INSERT INTO bears (name) VALUES ("Rowdy");
+INSERT INTO bears (name, age, gender) VALUES ("Tabitha", 6, "F");
+INSERT INTO bears (name) VALUES ("Sergeant Brown");
+INSERT INTO bears (name, age, gender) VALUES ("Melissa", 13, "F");
+INSERT INTO bears (name) VALUES ("Grinch");
+INSERT INTO bears (name, age, gender) VALUES ("Wendy", 6, "F");
+INSERT INTO bears (name, alive) VALUES (null, 1);
